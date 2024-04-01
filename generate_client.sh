c212d2a1541b8f62998c4f94fcfc0456d8ebf6a4
AlexName1 <at146@ya.ru>
2024-04-02 01:03:41 +0300

    add: script which adding "Package version" for generated client

--- generate_client.sh
+++ generate_client.sh
@@ -1,1 +1,15 @@
-openapi-generator-cli generate --generator-name python --library asyncio --input-spec https://sneakershopping.ru/api/v1/openapi.json --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output /home/alex_linux/my_code/swagger-client
+version=`cat VERSION`
+arrIN=(${version//./ })
+
+MAJOR=${arrIN[0]}
+MINOR=${arrIN[1]}
+PATCH=${arrIN[2]}
+
+MINOR=$((MINOR + 1))
+
+NEW_VERSION="$MAJOR.$MINOR.$PATCH"
+
+openapi-generator-cli generate --generator-name python --library asyncio --additional-properties packageVersion="$NEW_VERSION" --input-spec https://sneakershopping.ru/api/v1/openapi.json --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output /home/alex_linux/my_code/swagger-client
+
+echo "Current version: $NEW_VERSION"
+echo "$NEW_VERSION" > VERSION
