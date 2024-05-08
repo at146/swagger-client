e3fe837da4ba5687667a748a246eb19746effdcf
AlexName1 <at146@ya.ru>
2024-05-09 00:37:02 +0300

    add: parametr url_spec to generate client

--- generate_client.sh
+++ generate_client.sh
@@ -11,7 +11,10 @@ MINOR=$((MINOR + 1))
 
 NEW_VERSION="$MAJOR.$MINOR.$PATCH"
 
-OPENAPI_GENERATOR_VERSION=7.4.0 openapi-generator-cli generate --generator-name python --library asyncio --additional-properties packageVersion="$NEW_VERSION" --input-spec https://sneakershopping.ru/api/v1/openapi.json --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output /home/alex_linux/my_code/swagger-client
+url_spec=https://sneakershopping.ru/api/v1/openapi.json
+# url_spec=http://localhost:7773/api/v1/openapi.json
+
+OPENAPI_GENERATOR_VERSION=7.4.0 openapi-generator-cli generate --generator-name python --library asyncio --additional-properties packageVersion="$NEW_VERSION" --input-spec $url_spec --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output /home/alex_linux/my_code/swagger-client
 
 echo "Current version: $NEW_VERSION"
 echo "$NEW_VERSION" > VERSION
