387e5faaef7a4618e12e3c6a847828393ba854f5
AlexName1 <at146@ya.ru>
2024-05-09 00:57:36 +0300

    change: git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-# ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
-#
-# Usage example: /bin/sh ./git_push.sh wing328 openapi-petstore-perl "minor update" "gitlab.com"
 
 git_host="github.com"
 git_user_id="AlexName1"
 git_repo_id="swagger-client"
-release_note="minor update"
 version=`cat VERSION`
-
-# Adds the files in the local repository and stages them for commit.
-git add .
-
-# Commits the tracked changes and prepares them to be pushed to a remote repository.
-git commit -m "$release_note"
 
 git tag "$version"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push
 git push --tags
