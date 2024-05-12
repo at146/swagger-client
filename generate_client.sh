76e0ee6babab278718932c492490e40de089517d
AlexName1 <at146@ya.ru>
2024-05-12 18:14:22 +0300

    add: output_directory

--- generate_client.sh
+++ generate_client.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+url_spec=https://sneakershopping.ru/api/v1/openapi.json
+# url_spec=http://localhost:7773/api/v1/openapi.json
+
+# output_directory="/home/alex_linux/my_code/swagger-client"
+output_directory="/home/alex/code/swagger-client"
+
 version=`cat VERSION`
 arrIN=(${version//./ })
 
@@ -11,10 +17,7 @@ MINOR=$((MINOR + 1))
 
 NEW_VERSION="$MAJOR.$MINOR.$PATCH"
 
-url_spec=https://sneakershopping.ru/api/v1/openapi.json
-# url_spec=http://localhost:7773/api/v1/openapi.json
-
-OPENAPI_GENERATOR_VERSION=7.4.0 openapi-generator-cli generate --generator-name python --library asyncio --additional-properties packageVersion="$NEW_VERSION" --input-spec $url_spec --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output /home/alex_linux/my_code/swagger-client
+OPENAPI_GENERATOR_VERSION=7.4.0 openapi-generator-cli generate --generator-name python --library asyncio --additional-properties packageVersion="$NEW_VERSION" --input-spec $url_spec --git-repo-id "swagger-client" --git-user-id "AlexName1" --release-note "init" --package-name swagger_client --output $output_directory
 
 echo "Current version: $NEW_VERSION"
 echo "$NEW_VERSION" > VERSION
